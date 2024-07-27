573ab71686b6d802d58d7dfee903dfcec5d75420
Kareem Zaher <kareem.345@yahoo.com>
2024-07-28 01:24:41 +0300

    add webhooks env variable

--- app/Services/WebHooksService.php
+++ app/Services/WebHooksService.php
@@ -9,7 +9,7 @@ class WebHooksService
 {
     public function orderStatus($data)
     {
-        $response = Http::post('https://webhook.site/881a5e9e-6758-4712-b05e-a6fb6ce75c51', $data);
+        $response = Http::post(env('WEB_HOOKS_URL'), $data);
 
         if ($response->successful()) {
             return $response->json();
